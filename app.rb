f8628f65c113c5133d661db11bd85f88bd230d68
acparkgh <acpark123@gmail.com>
2021-03-16 23:26:13 -0400

    use attr_accessor

--- app.rb
+++ app.rb
@@ -4,29 +4,20 @@ class Cat
 
   attr_accessor :name, :flufiness
 
+  def initialize(species, gender)
+    @species = species 
+    @gender = gender 
+    @age = 1
+    @color = "white"
+  end
+
+  def sayWhat=(hotDog)
+    @name = hotDog
+  end
+
   def meow
     puts "Meow meow I am a pretty cat"
   end
-
-  # def set_flufiness(cat_fluffniss)
-  #   @flufiness = cat_fluffniss
-  # end
-
-  # def flufiness=(cat_flufiness)
-  #   @flufiness = cat_flufiness
-  # end
-
-  # def flufiness
-  #   return @flufiness
-  # end
-
-  # def name= (name)
-  #   @name = name
-  # end
-
-  # def name
-  #   @name
-  # end
 
   def introduce
     puts "Hi, my name is #{@name}."
@@ -34,8 +25,8 @@ class Cat
 
 end
 
-petey = Cat.new  # petey is an instance of "Cat" class
-garfield = Cat.new
+petey = Cat.new("cat", "male")  
+garfield = Cat.new("dog", "female")
 
 
 petey.meow
